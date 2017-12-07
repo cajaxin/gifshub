05c24d83442e7b17d0324e66945683790edad6e9
vnfontenele <vn.fontenele@gmail.com>
2017-12-07 00:32:50 -0200

    inicializando a função pipeline

--- pipeline.c
+++ pipeline.c
@@ -3,30 +3,31 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdarg.h>
 
-int main() {
-	int fd[2];
-	pid_t childpit;
-	
-	pipe(fd);
+int pipeline(FILE *fpin, FILE *fpout, const char *cmd1, cost char *cmd2, ...) {
+	/* variáveis iniciais */
+	int i;
+	int ncmds = 2; /* há pelo menos 2 comandos, obrigatoriamente (*cmd1 e *cmd2) */
+	char **command = NULL;
+	va_list cmds; /* tipo maluco pra usar o que vier nas reticências */
+	/* contar numero total de argumentos */
+	va_start(cmds, cmd2);
+	while (va_arg(cmds, char*) != NULL) {
+		ncmds++;
+	}
+	va_end(cmds);
+	/* gerar vetor com todos os argumentos e ficar livre de uma vez desse va_arg dos infernos */
+	command = malloc (ncmds * sizeof(char*));
+	command[0] = cmd1;
+	command[1] = cmd2;
+	va_start(cmds, cmd2);
+	for(i = 2; i < ncmds; i++) {
+		command[i] = va_arg(cmds, char*);
+	}
+	va_end(cmds);
+	/* agora a gente separa os homens dos meninos, digo, agora a gente começa a execução dos comandos */
 
-	if((childpit = fork() == -1)) {
-		perror("fork");
-		exit(1);
-	}
-
-	if(childpit == 0) {
-		/* Child process closes up input side of pipe */
-		close(fd[0]);
-		/* Send "string" through the output side of pipe */
-		
-	}
-	else {
-		/* Parent process closes up output side of pipe */
-		close(fd[1]);
-		/* Read in a string from the pipe */
-		
-	}
 
 	return 0;
 }
